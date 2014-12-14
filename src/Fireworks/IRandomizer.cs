e83a66e0aee890fdb8efecc2398eed4e7f78233e
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2014-12-14 21:18:37 +0300

    Added IRandomizer interface

---
+++ src/Fireworks/IRandomizer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fireworks
+{
+    public interface IRandomizer
+    {
+        double GetNext(double from, double to);
+    }
+}
