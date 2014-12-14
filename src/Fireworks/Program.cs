d3b397b07d25df1339346d05630f627557c88660
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2014-12-14 21:19:22 +0300

    Added dirty version of conventional Generate explosion sparks method
    Per the paper.

--- src/Fireworks/Program.cs
+++ src/Fireworks/Program.cs
@@ -12,4 +12,35 @@ namespace Fireworks
         {
         }
     }
+
+    public class Fireworks
+    {
+        public static void GenerateExplosionSparks(double[] fireworkCoords, double amplitude, IRandomizer randomizer, int dimensionsCount, double[] dimensionsMin, double[] dimensionsMax)
+        {
+            double[] sparkCoords = fireworkCoords;
+            double offsetDisplacement = amplitude * randomizer.GetNext(-1.0, 1.0);
+            int[] shiftCoord = new int[dimensionsCount];
+            for (int i = 0; i < dimensionsCount; i++)
+            {
+                shiftCoord[i] = (int)Math.Round(randomizer.GetNext(0.0, 1.0));
+            }
+
+            for (int i = 0; i < dimensionsCount; i++)
+            {
+                if (shiftCoord[i] == 1)
+                {
+                    sparkCoords[i] += offsetDisplacement;
+                    if (IsOutOfBounds(i, sparkCoords[i]))
+                    {
+                        sparkCoords[i] = dimensionsMin[i] + Math.Abs(sparkCoords[i]) % (dimensionsMax[i] - dimensionsMin[i]);
+                    }
+                }
+            }
+        }
+
+        public static bool IsOutOfBounds(int dimension, double coordValue)
+        {
+            return false;
+        }
+    }
 }
