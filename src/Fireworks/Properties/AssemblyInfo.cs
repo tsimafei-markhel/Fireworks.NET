ef6d441d8a6491742bd87eda88ba4dfd57827935
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2014-12-15 17:53:24 +0300

    Updated project properties & assembly info

--- src/Fireworks/Properties/AssemblyInfo.cs
+++ src/Fireworks/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Fireworks")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".NET implementation of Fireworks Algorithm (FWA)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Fireworks")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Tsimafei Markhel 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
