388d4164bdf3186e01a2f023d0ea453e125d4294
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2014-12-19 17:45:02 +0300

    Added Naming convention for implementations of ISparkGenerator interface

--- src/Fireworks/Explode/ISparkGenerator.cs
+++ src/Fireworks/Explode/ISparkGenerator.cs
@@ -2,6 +2,11 @@
 
 namespace Fireworks.Explode
 {
+	// Naming convention for implementations of this interface:
+	// - conventional (i.e. per 2010 paper) implementations are named w/o
+	//   any prefixes (e.g. ExplosionSparkGenerator)
+	// - enhanced/alternative implementations are named with corresponding
+	//   prefix (e.g. TODO)
 	public interface ISparkGenerator
 	{
 		Firework CreateSparks(Explosion explosion);
