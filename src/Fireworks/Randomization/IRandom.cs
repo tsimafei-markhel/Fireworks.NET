8fa53456e8e85db7c1cb80b599d990e5d0ceaed6
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2014-12-19 14:47:37 +0300

    Moved IRandom under Fireworks.Randomization namespace

---
+++ src/Fireworks/Randomization/IRandom.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Fireworks.Model;
+
+namespace Fireworks.Randomization
+{
+    public interface IRandom
+    {
+        Double GetNext(Double from, Double to);
+		Double GetNext(Range allowedRange);
+    }
+}
