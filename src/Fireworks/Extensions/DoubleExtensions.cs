c9c49fde5688f7a7b9ab9e6168acdad0357f5009
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2014-12-20 19:09:54 +0300

    Added a placeholder for Double extension methods
    To store helper stuff and also isolate Math.NET Numerics.

---
+++ src/Fireworks/Extensions/DoubleExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Fireworks.Extensions
+{
+    public static class DoubleExtensions
+    {
+        public static Boolean IsEqual(this Double left, Double right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Boolean IsSmaller(this Double left, Double right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Boolean IsSmallerOrEqual(this Double left, Double right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Boolean IsLarger(this Double left, Double right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Boolean IsLargerOrEqual(this Double left, Double right)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static String ToStringInvariant(this Double value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
