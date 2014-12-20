1941cae9ea1e7105a10ae8efa31be7198c7dc54b
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2014-12-20 23:49:12 +0300

    Added IContinuousDistribution interface

---
+++ src/Fireworks/Distributions/IContinuousDistribution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fireworks.Distributions
+{
+    /// <summary>
+    /// Continuous Univariate Probability Distribution.
+    /// </summary>
+    public interface IContinuousDistribution
+    {  
+        /// <summary>
+        /// Draws a random sample from the distribution.
+        /// </summary>
+        /// <returns>A sample from the distribution.</returns>
+        Double Sample();
+
+        /// <summary>
+        /// Draws a sequence of random samples from the distribution.
+        /// </summary>
+        /// <returns>An infinite sequence of samples from the distribution.</returns>
+        IEnumerable<Double> Samples();
+    }
+}
