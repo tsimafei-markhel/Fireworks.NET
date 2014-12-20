2b78b813d02d0f4adf7e054e7ae773b27dc35262
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2014-12-20 22:30:15 +0300

    Implemented DoubleExtensions methods

--- src/Fireworks/Extensions/DoubleExtensions.cs
+++ src/Fireworks/Extensions/DoubleExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using MathNet.Numerics;
 
 namespace Fireworks.Extensions
 {
@@ -6,32 +8,32 @@ namespace Fireworks.Extensions
     {
         public static Boolean IsEqual(this Double left, Double right)
         {
-            throw new NotImplementedException();
+            return left.AlmostEqual(right, Double.Epsilon);
         }
 
         public static Boolean IsSmaller(this Double left, Double right)
         {
-            throw new NotImplementedException();
+            return left.IsSmaller(right, Double.Epsilon);
         }
 
         public static Boolean IsSmallerOrEqual(this Double left, Double right)
         {
-            throw new NotImplementedException();
+            return left.IsSmaller(right, Double.Epsilon) || left.AlmostEqual(right, Double.Epsilon);
         }
 
         public static Boolean IsLarger(this Double left, Double right)
         {
-            throw new NotImplementedException();
+            return left.IsLarger(right, Double.Epsilon);
         }
 
         public static Boolean IsLargerOrEqual(this Double left, Double right)
         {
-            throw new NotImplementedException();
+            return left.IsLarger(right, Double.Epsilon) || left.AlmostEqual(right, Double.Epsilon);
         }
 
         public static String ToStringInvariant(this Double value)
         {
-            throw new NotImplementedException();
+            return value.ToString(CultureInfo.InvariantCulture);
         }
     }
 }
