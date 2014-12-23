8fddfe5f523ae35b0441a07c963710d98f8af826
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2014-12-23 17:44:22 +0300

    Added Double comparer that uses extension methods for comparison

---
+++ src/Fireworks/DoubleExtensionComparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Fireworks.Extensions;
+
+namespace Fireworks
+{
+	public class DoubleExtensionComparer : IComparer<Double>
+	{
+		public Int32 Compare(Double x, Double y)
+		{
+			if (x.IsLess(y))
+			{
+				return -1;
+			}
+			else if (x.IsGreater(y))
+			{
+				return 1;
+			}
+
+			return 0;
+		}
+	}
+}
