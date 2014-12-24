939f05c79fb5e9cafcaa5b3a99d96140c5cf129d
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2014-12-24 13:22:37 +0300

    Added datamodel-oriented method to IDistance interface

--- src/Fireworks/Distance/IDistance.cs
+++ src/Fireworks/Distance/IDistance.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using Fireworks.Model;
 
 namespace Fireworks.Distance
 {
 	public interface IDistance
 	{
 		Double Calculate(Double[] first, Double second);
+		Double Calculate(Firework first, Firework second);
 	}
 }
