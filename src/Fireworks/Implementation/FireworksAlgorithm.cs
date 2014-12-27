90655a22fb535a7751f50f03a6e4f75bad46a666
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2014-12-27 23:12:53 +0300

    Added a draft for 2010 paper algorithm modification

---
+++ src/Fireworks/Implementation/FireworksAlgorithm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Fireworks.Model;
+using Fireworks.Problems;
+
+namespace Fireworks.Implementation
+{
+    public class FireworksAlgorithm : IFireworksAlgorithm
+    {
+        public Firework Solve(Problem problem, AlgorithmSetup setup)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
