7a36c8a3e1a8a17c1f6d768a86bd1829433aeefc
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2014-12-27 23:11:54 +0300

    Added algorithm interface: different modifications are to implement it

---
+++ src/Fireworks/Implementation/IFireworksAlgorithm.cs
@@ -0,0 +1,10 @@
+﻿using Fireworks.Model;
+using Fireworks.Problems;
+
+namespace Fireworks.Implementation
+{
+    public interface IFireworksAlgorithm
+    {
+        Firework Solve(Problem problem, AlgorithmSetup setup);
+    }
+}
