e0d6f603621cd6ffeae743cd184b05e7b3aa5d59
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-01-02 14:04:41 +0300

    Now initial spark generator supports initial dimension ranges

--- src/FireworksNet/Explode/InitialSparkGenerator.cs
+++ src/FireworksNet/Explode/InitialSparkGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using FireworksNet.Model;
 using FireworksNet.Randomization;
 
@@ -8,12 +9,12 @@ namespace FireworksNet.Explode
 	public class InitialSparkGenerator : SparkGenerator
 	{
 		private readonly IEnumerable<Dimension> dimensions;
+        private readonly IDictionary<Dimension, Range> initialDimensionRanges;
 		private readonly IRandom randomizer;
 
 		public override FireworkType GeneratedSparkType { get { return FireworkType.Initial; } }
 
-        // TODO: Take initial dimension ranges into account
-		public InitialSparkGenerator(IEnumerable<Dimension> dimensions, IRandom randomizer)
+		public InitialSparkGenerator(IEnumerable<Dimension> dimensions, IDictionary<Dimension, Range> initialDimensionRanges, IRandom randomizer)
 		{
 			if (dimensions == null)
 			{
@@ -26,18 +27,44 @@ namespace FireworksNet.Explode
 			}
 
 			this.dimensions = dimensions;
+            this.initialDimensionRanges = SetInitialDimensionRanges(dimensions, initialDimensionRanges);
 			this.randomizer = randomizer;
 		}
+
+        public InitialSparkGenerator(IEnumerable<Dimension> dimensions, IRandom randomizer)
+            : this(dimensions, null, randomizer)
+        {
+        }
 
 		public override Firework CreateSpark(Explosion explosion)
 		{
 			Firework spark = new Firework(GeneratedSparkType, 0);
 			foreach (Dimension dimension in dimensions)
 			{
-				spark.Coordinates[dimension] = dimension.VariationRange.Minimum + randomizer.GetNext(0.0, 1.0) * dimension.VariationRange.Length;
+                Range dimensionRange = initialDimensionRanges[dimension];
+                spark.Coordinates[dimension] = dimensionRange.Minimum + randomizer.GetNext(0.0, 1.0) * dimensionRange.Length;
 			}
 
 			return spark;
 		}
+
+        private static IDictionary<Dimension, Range> SetInitialDimensionRanges(IEnumerable<Dimension> dimensions, IDictionary<Dimension, Range> initialDimensionRanges)
+        {
+            if (initialDimensionRanges != null)
+            {
+                // TODO: Need validation to make sure dimensions and initialDimensionRanges contain the same Dimension instances
+                return initialDimensionRanges;
+            }
+            else
+            {
+                Dictionary<Dimension, Range> dimensionRanges = new Dictionary<Dimension, Range>(dimensions.Count());
+                foreach (Dimension dimension in dimensions)
+                {
+                    dimensionRanges.Add(dimension, dimension.VariationRange);
+                }
+
+                return dimensionRanges;
+            }
+        }
 	}
 }
