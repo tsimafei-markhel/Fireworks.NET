39daf41da7cc7a72b92b5f17c88b310f36403589
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-01-03 00:59:55 +0300

    Added a stop condition: by step count.

---
+++ src/FireworksNet/Problems/StepCountStopCondition.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FireworksNet.Model;
+
+namespace FireworksNet.Problems
+{
+    public class StepCountStopCondition : IStopCondition
+    {
+        private readonly int maxStepCount;
+
+        public StepCountStopCondition(Int32 maxStepCount)
+        {
+            if (maxStepCount < 0)
+            {
+                throw new ArgumentOutOfRangeException("maxStepCount");
+            }
+
+            this.maxStepCount = maxStepCount;
+        }
+
+        public Boolean ShouldStop(IEnumerable<Firework> currentFireworks)
+        {
+            if (currentFireworks == null)
+            {
+                throw new ArgumentNullException("currentFireworks");
+            }
+
+            int maxCurrentStep = currentFireworks.Max(fw => fw.BirthStepNumber); // TODO: This seems to be quite inefficient...
+            return maxCurrentStep >= maxStepCount;
+        }
+    }
+}
