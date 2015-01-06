1bb2895bfac489180aca70f324af1918a585a298
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-01-06 23:23:20 +0300

    Added a System.Random extension to return an enumerable of random Int32s

--- src/FireworksNet/Extensions/RandomExtensions.cs
+++ src/FireworksNet/Extensions/RandomExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using FireworksNet.Model;
 
 namespace FireworksNet.Extensions
@@ -23,6 +24,24 @@ namespace FireworksNet.Extensions
             while (!gotCorrectValue);
 
             return correctValue;
+        }
+
+        /// <summary>
+        /// Returns an enumerable of random integer numbers.
+        /// </summary>
+        /// <param name="random">The random number generator.</param>
+        /// <param name="neededValuesNumber">The amount of values to be generated.</param>
+        /// <param name="minInclusive">Lower bound, inclusive.</param>
+        /// <param name="maxExclusive">Upper bound, exclusive.</param>
+        public static IEnumerable<Int32> NextInt32s(this System.Random random, Int32 neededValuesNumber, Int32 minInclusive, Int32 maxExclusive)
+        {
+            List<int> result = new List<int>(neededValuesNumber);
+            for (int i = 0; i < neededValuesNumber; i++)
+            {
+                result.Add(random.Next(minInclusive, maxExclusive));
+            }
+
+            return result;
         }
 
         private static Double NextDoubleInternal(System.Random random, Double from, Double intervalLength)
