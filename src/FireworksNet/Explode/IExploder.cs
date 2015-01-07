c5ac48a4bc8c0eb93712e72df64721552548e1e9
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-01-07 19:37:25 +0300

    TODO for future

--- src/FireworksNet/Explode/IExploder.cs
+++ src/FireworksNet/Explode/IExploder.cs
@@ -5,6 +5,7 @@ namespace FireworksNet.Explode
 {
     public interface IExploder
     {
+        // TODO: Make it per-run, not per-step so that one instance per an algorithm run is created
         Explosion Explode(Firework epicenter, Int32 currentStepNumber);
     }
 }
