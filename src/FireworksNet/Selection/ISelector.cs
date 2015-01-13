6d485b94c1cb8c405e4618a2866c00c37d2cff96
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-01-13 17:25:46 +0300

    Missing changes in ISelector and csproj

--- src/FireworksNet/Selection/ISelector.cs
+++ src/FireworksNet/Selection/ISelector.cs
@@ -7,6 +7,6 @@ namespace FireworksNet.Selection
 	public interface ISelector
 	{
 		IEnumerable<Firework> Select(IEnumerable<Firework> from);
-		IEnumerable<Firework> Select(IEnumerable<Firework> from, Int32 amount);
+		IEnumerable<Firework> Select(IEnumerable<Firework> from, Int32 numberToSelect);
 	}
 }
