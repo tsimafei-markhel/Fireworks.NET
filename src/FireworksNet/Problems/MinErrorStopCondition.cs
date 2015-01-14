6f54a599c4c9f93f5e19f0c4ef21e2c895dce487
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-01-14 17:53:41 +0300

    Implemented MinErrorStopCondition

--- src/FireworksNet/Problems/MinErrorStopCondition.cs
+++ src/FireworksNet/Problems/MinErrorStopCondition.cs
@@ -9,38 +9,51 @@ namespace FireworksNet.Problems
     public class MinErrorStopCondition : IStopCondition
     {
         private readonly Double[] knownBest;
-        private readonly Double minError;
-        private readonly IDistance distanceCalculator;
+		private readonly Func<IEnumerable<Firework>, Firework> bestFireworkSelector;
+		private readonly IDistance distanceCalculator;
+		private readonly Double minError;
 
-        public MinErrorStopCondition(Double[] knownBest, Double minError, IDistance distanceCalculator)
-        {
-            if (knownBest == null)
-            {
-                throw new ArgumentNullException("knownBest");
-            }
+		public MinErrorStopCondition(Double[] knownBest, Func<IEnumerable<Firework>, Firework> bestFireworkSelector, IDistance distanceCalculator, Double minError)
+		{
+			if (knownBest == null)
+			{
+				throw new ArgumentNullException("knownBest");
+			}
 
-            if (Double.IsNaN(minError) || Double.IsInfinity(minError))
-            {
-                throw new ArgumentOutOfRangeException("knownBest");
-            }
+			if (bestFireworkSelector == null)
+			{
+				throw new ArgumentNullException("bestFireworkSelector");
+			}
 
-            if (distanceCalculator == null)
-            {
-                throw new ArgumentNullException("distanceCalculator");
-            }
+			if (distanceCalculator == null)
+			{
+				throw new ArgumentNullException("distanceCalculator");
+			}
 
-            this.knownBest = knownBest;
-            this.minError = minError;
-            this.distanceCalculator = distanceCalculator;
+			if (Double.IsNaN(minError) || Double.IsInfinity(minError))
+			{
+				throw new ArgumentOutOfRangeException("knownBest");
+			}
+
+			this.knownBest = knownBest;
+			this.bestFireworkSelector = bestFireworkSelector;
+			this.distanceCalculator = distanceCalculator;
+			this.minError = minError;
+		}
+
+		public MinErrorStopCondition(Double[] knownBest, Func<IEnumerable<Firework>, Firework> bestFireworkSelector, IDistance distanceCalculator)
+			: this(knownBest, bestFireworkSelector, distanceCalculator, double.Epsilon)
+		{
         }
 
         public Boolean ShouldStop(IEnumerable<Firework> currentFireworks)
         {
-            throw new NotImplementedException();
+			if (currentFireworks == null)
+			{
+				throw new ArgumentNullException("currentFireworks");
+			}
 
-            // TODO: Looking for best existing firework on each call may be inefficient...
-
-            Firework bestFirework = null; // TODO: call something to find best existing solution
+			Firework bestFirework = bestFireworkSelector(currentFireworks); // TODO: Looking for best existing firework on each call may be inefficient...
             Double error = distanceCalculator.Calculate(bestFirework, knownBest);
 
             return error.IsLessOrEqual(minError);
