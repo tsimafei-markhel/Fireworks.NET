33ffd140dbf358ff2df17138d2fd327f4bf20857
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-01-15 15:36:39 +0300

    TODO for future

--- src/FireworksNet/DoubleExtensionComparer.cs
+++ src/FireworksNet/DoubleExtensionComparer.cs
@@ -4,6 +4,7 @@ using FireworksNet.Extensions;
 
 namespace FireworksNet
 {
+	// TODO: Find proper namespace for this one
 	public class DoubleExtensionComparer : IComparer<Double>
 	{
 		public Int32 Compare(Double x, Double y)
