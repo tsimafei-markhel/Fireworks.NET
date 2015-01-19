621a71ff0e085527206bdcbe0842f1ae70c211a5
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-01-19 17:52:24 +0300

    Some work on distance calculation

--- src/FireworksNet/Distances/EuclideanDistance.cs
+++ src/FireworksNet/Distances/EuclideanDistance.cs
@@ -6,6 +6,7 @@ using MathNet.Numerics;
 
 namespace FireworksNet.Distances
 {
+	// TODO: Once another distance calculator is required, come up with base class.
 	public class EuclideanDistance : IDistance
 	{
 		private readonly IEnumerable<Dimension> dimensions;
@@ -22,43 +23,100 @@ namespace FireworksNet.Distances
 
 		public Double Calculate(Double[] first, Double[] second)
 		{
+			if (first == null)
+			{
+				throw new ArgumentNullException("first");
+			}
+
+			if (second == null)
+			{
+				throw new ArgumentNullException("second");
+			}
+
 			return Distance.Euclidean(first, second);
 		}
 
 		public Double Calculate(Firework first, Firework second)
 		{
+			if (first == null)
+			{
+				throw new ArgumentNullException("first");
+			}
+
+			if (second == null)
+			{
+				throw new ArgumentNullException("second");
+			}
+
 			if (first == second)
 			{
 				return 0.0;
 			}
 
-			// TODO: Move this logic to base abstract class
-			double[] firstCoords = new double[dimensions.Count()];
-			double[] secondCoords = new double[dimensions.Count()];
+			double[] firstCoordinates;
+			double[] secondCoordinates;
+			GetCoordinates(first, second, out firstCoordinates, out secondCoordinates);
 
-			int dimensionCounter = 0;
-			foreach (Dimension dimension in dimensions)
-			{
-				firstCoords[dimensionCounter] = first.Coordinates[dimension];
-				secondCoords[dimensionCounter] = second.Coordinates[dimension];
-				dimensionCounter++;
-			}
-
-			return Calculate(firstCoords, secondCoords);
+			return Calculate(firstCoordinates, secondCoordinates);
 		}
 
         public Double Calculate(Firework first, Double[] second)
         {
-            // TODO: Move this logic to base abstract class
-            double[] firstCoords = new double[dimensions.Count()];
-            int dimensionCounter = 0;
-            foreach (Dimension dimension in dimensions)
-            {
-                firstCoords[dimensionCounter] = first.Coordinates[dimension];
-                dimensionCounter++;
-            }
+			if (first == null)
+			{
+				throw new ArgumentNullException("first");
+			}
 
-            return Calculate(firstCoords, second);
+			if (second == null)
+			{
+				throw new ArgumentNullException("second");
+			}
+
+			return Calculate(GetCoordinates(first), second);
         }
+
+		private Double[] GetCoordinates(Firework firework)
+		{
+			System.Diagnostics.Debug.Assert(dimensions != null, "Dimension collection is null");
+			System.Diagnostics.Debug.Assert(firework != null, "Firework is null");
+			System.Diagnostics.Debug.Assert(firework.Coordinates != null, "Firework coordinate collection is null");
+
+			double[] coordinates = new double[dimensions.Count()];
+
+			int dimensionCounter = 0;
+			foreach (Dimension dimension in dimensions)
+			{
+				System.Diagnostics.Debug.Assert(dimension != null, "Dimension is null");
+
+				coordinates[dimensionCounter] = firework.Coordinates[dimension];
+
+				dimensionCounter++;
+			}
+
+			return coordinates;
+		}
+
+		private void GetCoordinates(Firework firstFirework, Firework secondFirework, out Double[] firstCoordinates, out Double[] secondCoordinates)
+		{
+			System.Diagnostics.Debug.Assert(dimensions != null, "Dimension collection is null");
+			System.Diagnostics.Debug.Assert(firstFirework != null, "First firework is null");
+			System.Diagnostics.Debug.Assert(secondFirework != null, "Second firework is null");
+			System.Diagnostics.Debug.Assert(firstFirework.Coordinates != null, "First firework coordinate collection is null");
+			System.Diagnostics.Debug.Assert(secondFirework.Coordinates != null, "Second firework coordinate collection is null");
+
+			firstCoordinates = new double[dimensions.Count()];
+			secondCoordinates = new double[dimensions.Count()];
+
+			int dimensionCounter = 0;
+			foreach (Dimension dimension in dimensions)
+			{
+				System.Diagnostics.Debug.Assert(dimension != null, "Dimension is null");
+
+				firstCoordinates[dimensionCounter] = firstFirework.Coordinates[dimension];
+				secondCoordinates[dimensionCounter] = secondFirework.Coordinates[dimension];
+
+				dimensionCounter++;
+			}
+		}
     }
 }
