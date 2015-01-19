a9037ee7ebd1d5f556c9c41dcaa09161e778f2bf
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-01-19 18:07:42 +0300

    Added a pilot benchmark problem: Sphere, as described in 2010 paper

--- src/FireworksNet/Problems/Benchmark/BenchmarkProblem.cs
+++ src/FireworksNet/Problems/Benchmark/BenchmarkProblem.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using FireworksNet.Model;
 
 namespace FireworksNet.Problems.Benchmark
 {
     public class BenchmarkProblem : Problem
     {
+		public readonly Lazy<BenchmarkProblem> Sphere2010 = new Lazy<BenchmarkProblem>(InitializeSphere2010);
+
+		// TODO: May need coordinates instead of value...
         public Double KnownSolution { get; private set; }
 
         public BenchmarkProblem(IEnumerable<Dimension> dimensions, IDictionary<Dimension, Range> initialDimensionRanges, Func<IDictionary<Dimension, Double>, Double> targetFunction, Double knownSolution, IStopCondition stopCondition, ProblemTarget target)
@@ -28,5 +32,26 @@ namespace FireworksNet.Problems.Benchmark
             : this(dimensions, null, targetFunction, knownSolution, stopCondition, ProblemTarget.Minimum)
         {
         }
+
+		private static BenchmarkProblem InitializeSphere2010()
+		{
+			Dimension[] dimensions = new Dimension[30];
+			IDictionary<Dimension, Range> initialDimensionRanges = new Dictionary<Dimension, Range>(30);
+			for (int i = 0; i < 30; i++ )
+			{
+				dimensions[i] = new Dimension(new Range(-100.0, 100.0));
+				initialDimensionRanges.Add(dimensions[i], new Range(30.0, 50.0));
+			}
+
+			Func<IDictionary<Dimension, double>, double> func = new Func<IDictionary<Dimension, double>, double>(
+				(c) => {
+					return c.Values.Sum(v => Math.Pow(v, 2.0));
+				}
+			);
+
+			// TODO: In 2010 paper, stop condition was not number of steps
+			// TODO: Need 'max number of quality function evaluations' stop condition
+			return new BenchmarkProblem(dimensions, initialDimensionRanges, func, 0.0, new StepCountStopCondition(10), ProblemTarget.Minimum);
+		}
     }
 }
