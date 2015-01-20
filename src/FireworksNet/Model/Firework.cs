90f80c3d7147b5f4fcd10ed1b3e25626de99802e
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-01-20 13:44:49 +0300

    Added Solution model class
    Is is used as a base for Firework class. It will be used in algorithm
    implementations and in benchmark problems.

--- src/FireworksNet/Model/Firework.cs
+++ src/FireworksNet/Model/Firework.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace FireworksNet.Model
 {
-	public class Firework
+	public class Firework : Solution
 	{
 		public String Id { get; private set; }
 
@@ -11,32 +11,22 @@ namespace FireworksNet.Model
 
 		public Int32 BirthStepNumber { get; private set; }
 
-		public Double Quality { get; set; }
-
-		public IDictionary<Dimension, Double> Coordinates { get; private set; }
-
-		public Firework(FireworkType type, Int32 birthStepNumber)
-			: this(type, birthStepNumber, new Dictionary<Dimension, Double>())
-		{
-		}
-
 		public Firework(FireworkType type, Int32 birthStepNumber, IDictionary<Dimension, Double> coordinates)
+			: base(coordinates)
 		{
 			if (birthStepNumber < 0)
 			{
 				throw new ArgumentOutOfRangeException("birthStepNumber");
 			}
 
-			if (coordinates == null)
-			{
-				throw new ArgumentNullException("coordinates");
-			}
-
 			Id = Guid.NewGuid().ToString();
 			Type = type;
 			BirthStepNumber = birthStepNumber;
-			Quality = Double.NaN;
-			Coordinates = new Dictionary<Dimension, Double>(coordinates);
+		}
+
+		public Firework(FireworkType type, Int32 birthStepNumber)
+			: this(type, birthStepNumber, new Dictionary<Dimension, Double>())
+		{
 		}
 	}
 }
---
+++ src/FireworksNet/Model/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FireworksNet.Model
+{
+	public class Solution
+	{
+		public IDictionary<Dimension, Double> Coordinates { get; private set; }
+
+		public Double Quality { get; set; }
+
+		public Solution(IDictionary<Dimension, Double> coordinates, Double quality)
+		{
+			if (coordinates == null)
+			{
+				throw new ArgumentNullException("coordinates");
+			}
+
+			if (double.IsInfinity(quality))
+			{
+				throw new ArgumentOutOfRangeException("quality");
+			}
+
+			Coordinates = new Dictionary<Dimension, Double>(coordinates);
+			Quality = quality;
+		}
+
+		public Solution(IDictionary<Dimension, Double> coordinates)
+			: this(coordinates, Double.NaN)
+		{
+		}
+	}
+}
