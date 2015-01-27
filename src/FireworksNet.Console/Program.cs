dff5eb0cd37acc3e2493b14d1b86a141f03d0646
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-01-27 15:38:36 +0300

    Some draft for future.

--- src/FireworksNet.Console/Program.cs
+++ src/FireworksNet.Console/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using FireworksNet.Algorithm.Implementation;
+using FireworksNet.Model;
+using FireworksNet.Problems;
+using FireworksNet.Problems.Benchmark;
 
 namespace FireworksNet.Console
 {
@@ -7,6 +11,20 @@ namespace FireworksNet.Console
 		private static void Main(string[] args)
 		{
 			throw new NotImplementedException();
+
+			//FireworksAlgorithmSettings fwaSettings = new FireworksAlgorithmSettings()
+			//{
+			//	LocationsNumber = 5,
+			//	ExplosionSparksNumberModifier = 50.0,
+			//	ExplosionSparksNumberLowerBound = 0.04,
+			//	ExplosionSparksNumberUpperBound = 0.8,
+			//	ExplosionSparksMaximumAmplitude = 40.0,
+			//	SpecificSparksNumber = 5,
+			//	SpecificSparksPerExplosionNumber = 1
+			//};
+
+			//FireworksAlgorithm fwa = new FireworksAlgorithm(BenchmarkProblems.Sphere2010, fwaSettings);
+			//Solution solution = fwa.Solve();
 		}
 	}
 }
