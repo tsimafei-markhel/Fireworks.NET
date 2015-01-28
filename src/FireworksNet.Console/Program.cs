6c41e59be13e4c17fac52fd10cad015a75c658b7
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-01-28 10:58:58 +0300

    Very basic example of how to use Fireworks.NET library

--- src/FireworksNet.Console/Program.cs
+++ src/FireworksNet.Console/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using FireworksNet.Algorithm.Implementation;
+﻿using FireworksNet.Algorithm.Implementation;
 using FireworksNet.Model;
-using FireworksNet.Problems;
 using FireworksNet.Problems.Benchmark;
 
 namespace FireworksNet.Console
@@ -10,21 +8,19 @@ namespace FireworksNet.Console
 	{
 		private static void Main(string[] args)
 		{
-			throw new NotImplementedException();
+			FireworksAlgorithmSettings fwaSettings = new FireworksAlgorithmSettings()
+			{
+				LocationsNumber = 5,
+				ExplosionSparksNumberModifier = 50.0,
+				ExplosionSparksNumberLowerBound = 0.04,
+				ExplosionSparksNumberUpperBound = 0.8,
+				ExplosionSparksMaximumAmplitude = 40.0,
+				SpecificSparksNumber = 5,
+				SpecificSparksPerExplosionNumber = 1
+			};
 
-			//FireworksAlgorithmSettings fwaSettings = new FireworksAlgorithmSettings()
-			//{
-			//	LocationsNumber = 5,
-			//	ExplosionSparksNumberModifier = 50.0,
-			//	ExplosionSparksNumberLowerBound = 0.04,
-			//	ExplosionSparksNumberUpperBound = 0.8,
-			//	ExplosionSparksMaximumAmplitude = 40.0,
-			//	SpecificSparksNumber = 5,
-			//	SpecificSparksPerExplosionNumber = 1
-			//};
-
-			//FireworksAlgorithm fwa = new FireworksAlgorithm(BenchmarkProblems.Sphere2010, fwaSettings);
-			//Solution solution = fwa.Solve();
+			FireworksAlgorithm fwa = new FireworksAlgorithm(Sphere2010.Create(), fwaSettings);
+			Solution solution = fwa.Solve();
 		}
 	}
 }
