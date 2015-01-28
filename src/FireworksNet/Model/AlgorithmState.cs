0a869d90ef50e964ce7ec8a3320b9dc91293dd48
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-01-28 16:22:33 +0300

    Added a class to store algorithm state.

---
+++ src/FireworksNet/Model/AlgorithmState.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FireworksNet.Model
+{
+	/// <summary>
+	/// Stores current algorithm state.
+	/// </summary>
+	/// <remarks>This class is not thread-safe.</remarks>
+	public class AlgorithmState
+	{
+		private IEnumerable<Firework> fireworks;
+		private int stepNumber;
+		private Solution bestSolution;
+
+		/// <summary>
+		/// Gets or sets a collection of current fireworks.
+		/// </summary>
+		/// <exception cref="System.ArgumentNullException"> if value is null.</exception>
+		public IEnumerable<Firework> Fireworks
+		{
+			get
+			{
+				return fireworks;
+			}
+
+			set
+			{
+				if (value == null)
+				{
+					throw new ArgumentNullException("value");
+				}
+
+				fireworks = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the step number.
+		/// </summary>
+		/// <exception cref="System.ArgumentOutOfRangeException"> if value is less than 0.</exception>
+		public int StepNumber
+		{
+			get
+			{
+				return stepNumber;
+			}
+
+			set
+			{
+				if (value < 0)
+				{
+					throw new ArgumentOutOfRangeException("value");
+				}
+
+				stepNumber = value;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the best solution among <see cref="AlgorithmState.Fireworks"/>.
+		/// </summary>
+		/// <exception cref="System.ArgumentNullException"> if value is null.</exception>
+		public Solution BestSolution
+		{
+			get
+			{
+				return bestSolution;
+			}
+
+			set
+			{
+				if (value == null)
+				{
+					throw new ArgumentNullException("value");
+				}
+
+				bestSolution = value;
+			}
+		}
+	}
+}
