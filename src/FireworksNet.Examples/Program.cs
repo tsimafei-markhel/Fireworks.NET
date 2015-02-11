328b53f32f2627a3e48af9e9e616ce84a38a519c
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-02-11 17:14:36 +0300

    Example - converted tabs to spaces

--- src/FireworksNet.Examples/Program.cs
+++ src/FireworksNet.Examples/Program.cs
@@ -5,40 +5,40 @@ using FireworksNet.StopConditions;
 
 namespace FireworksNet.Examples
 {
-	public class Program
-	{
-		private static void Main(string[] args)
-		{
-			// 1. Define a problem to solve
-			Sphere2010 problem = Sphere2010.Create();
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // 1. Define a problem to solve
+            Sphere2010 problem = Sphere2010.Create();
 
-			// 2. Setup algorithm stop condition
-			CounterStopCondition stopCondition = new CounterStopCondition(10000);
-			problem.QualityCalculated += stopCondition.IncrementCounter;
+            // 2. Setup algorithm stop condition
+            CounterStopCondition stopCondition = new CounterStopCondition(10000);
+            problem.QualityCalculated += stopCondition.IncrementCounter;
 
-			// 3. Initialize algorithm run settings
-			FireworksAlgorithmSettings settings = new FireworksAlgorithmSettings()
-			{
-				LocationsNumber = 5,
-				ExplosionSparksNumberModifier = 50.0,
-				ExplosionSparksNumberLowerBound = 0.04,
-				ExplosionSparksNumberUpperBound = 0.8,
-				ExplosionSparksMaximumAmplitude = 40.0,
-				SpecificSparksNumber = 5,
-				SpecificSparksPerExplosionNumber = 1
-			};
+            // 3. Initialize algorithm run settings
+            FireworksAlgorithmSettings settings = new FireworksAlgorithmSettings()
+            {
+                LocationsNumber = 5,
+                ExplosionSparksNumberModifier = 50.0,
+                ExplosionSparksNumberLowerBound = 0.04,
+                ExplosionSparksNumberUpperBound = 0.8,
+                ExplosionSparksMaximumAmplitude = 40.0,
+                SpecificSparksNumber = 5,
+                SpecificSparksPerExplosionNumber = 1
+            };
 
-			// 4. Instantiate desired implementation of the algorithm (per 2010 paper in this case)
-			FireworksAlgorithm fwa2010 = new FireworksAlgorithm(problem, stopCondition, settings);
+            // 4. Instantiate desired implementation of the algorithm (per 2010 paper in this case)
+            FireworksAlgorithm fwa2010 = new FireworksAlgorithm(problem, stopCondition, settings);
 
-			// 5. Finally, find a solution
-			Solution solution = fwa2010.Solve();
+            // 5. Finally, find a solution
+            Solution solution = fwa2010.Solve();
 
-			// TODO: Ideas for 'usage examples':
-			//       1. Simple run: alg settings, one of the benchmark problems, get the solution
-			//       2. Define user problem
-			//       3. Composite stop condition
-			//       4. Capture states after each step
-		}
-	}
+            // TODO: Ideas for 'usage examples':
+            //       1. Simple run: alg settings, one of the benchmark problems, get the solution
+            //       2. Define user problem
+            //       3. Composite stop condition
+            //       4. Capture states after each step
+        }
+    }
 }
