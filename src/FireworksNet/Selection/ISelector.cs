2e6a3a5661e22d47c720a1c37a3d2f86539da295
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-02-11 17:05:45 +0300

    Selection - added XML comments, converted tabs to spaces, used 'this' where necessary.

--- src/FireworksNet/Selection/ISelector.cs
+++ src/FireworksNet/Selection/ISelector.cs
@@ -3,9 +3,30 @@ using FireworksNet.Model;
 
 namespace FireworksNet.Selection
 {
-	public interface ISelector
-	{
-		IEnumerable<Firework> Select(IEnumerable<Firework> from);
+    /// <summary>
+    /// Contains logic for selecting a subset of <see cref="Firework"/>s
+    /// from a given set according to some rule.
+    /// </summary>
+    public interface ISelector
+    {
+        /// <summary>
+        /// Selects some predefined number of <see cref="Firework"/>s from
+        /// the <paramref name="from"/> collection.
+        /// </summary>
+        /// <param name="from">A collection to select <see cref="Firework"/>s
+        /// from.</param>
+        /// <returns>A subset of <see cref="Firework"/>s.</returns>
+        IEnumerable<Firework> Select(IEnumerable<Firework> from);
+
+        /// <summary>
+        /// Selects <paramref name="numberToSelect"/> <see cref="Firework"/>s from
+        /// the <paramref name="from"/> collection.
+        /// </summary>
+        /// <param name="from">A collection to select <see cref="Firework"/>s
+        /// from.</param>
+        /// <param name="numberToSelect">The number of <see cref="Firework"/>s
+        /// to select.</param>
+        /// <returns>A subset of <see cref="Firework"/>s.</returns>
         IEnumerable<Firework> Select(IEnumerable<Firework> from, int numberToSelect);
-	}
+    }
 }
--- src/FireworksNet/Selection/LocationSelector.cs
+++ src/FireworksNet/Selection/LocationSelector.cs
@@ -7,86 +7,86 @@ using FireworksNet.Model;
 
 namespace FireworksNet.Selection
 {
-	public class LocationSelector : ISelector
-	{
-		private readonly IDistance distanceCalculator;
-		private readonly Func<IEnumerable<Firework>, Firework> bestFireworkSelector;
+    public class LocationSelector : ISelector
+    {
+        private readonly IDistance distanceCalculator;
+        private readonly Func<IEnumerable<Firework>, Firework> bestFireworkSelector;
         private readonly int locationsNumber;
 
         public LocationSelector(IDistance distanceCalculator, Func<IEnumerable<Firework>, Firework> bestFireworkSelector, int locationsNumber)
-		{
-			if (distanceCalculator == null)
-			{
-				throw new ArgumentNullException("distanceCalculator");
-			}
+        {
+            if (distanceCalculator == null)
+            {
+                throw new ArgumentNullException("distanceCalculator");
+            }
 
-			if (bestFireworkSelector == null)
-			{
-				throw new ArgumentNullException("bestFireworkSelector");
-			}
+            if (bestFireworkSelector == null)
+            {
+                throw new ArgumentNullException("bestFireworkSelector");
+            }
 
-			if (locationsNumber < 0)
-			{
-				throw new ArgumentOutOfRangeException("locationsNumber");
-			}
+            if (locationsNumber < 0)
+            {
+                throw new ArgumentOutOfRangeException("locationsNumber");
+            }
 
-			this.locationsNumber = locationsNumber;
-			this.distanceCalculator = distanceCalculator;
-			this.bestFireworkSelector = bestFireworkSelector;
-		}
+            this.locationsNumber = locationsNumber;
+            this.distanceCalculator = distanceCalculator;
+            this.bestFireworkSelector = bestFireworkSelector;
+        }
 
-		public LocationSelector(IDistance distanceCalculator, Func<IEnumerable<Firework>, Firework> bestFireworkSelector)
-			: this(distanceCalculator, bestFireworkSelector, 0)
-		{
-		}
+        public LocationSelector(IDistance distanceCalculator, Func<IEnumerable<Firework>, Firework> bestFireworkSelector)
+            : this(distanceCalculator, bestFireworkSelector, 0)
+        {
+        }
 
-		public virtual IEnumerable<Firework> Select(IEnumerable<Firework> from)
-		{
-			return Select(from, locationsNumber);
-		}
+        public virtual IEnumerable<Firework> Select(IEnumerable<Firework> from)
+        {
+            return this.Select(from, this.locationsNumber);
+        }
 
-		// Always returns new collection
+        // Always returns new collection
         public virtual IEnumerable<Firework> Select(IEnumerable<Firework> from, int numberToSelect)
-		{
-			if (from == null)
-			{
-				throw new ArgumentNullException("from");
-			}
+        {
+            if (from == null)
+            {
+                throw new ArgumentNullException("from");
+            }
 
-			if (numberToSelect < 0)
-			{
-				throw new ArgumentOutOfRangeException("numberToSelect");
-			}
+            if (numberToSelect < 0)
+            {
+                throw new ArgumentOutOfRangeException("numberToSelect");
+            }
 
-			if (numberToSelect > from.Count())
-			{
-				// At some point, we may need to return just as much as we have
-				// instead of throwing an exception.
-				throw new ArgumentOutOfRangeException("numberToSelect");
-			}
+            if (numberToSelect > from.Count())
+            {
+                // At some point, we may need to return just as much as we have
+                // instead of throwing an exception.
+                throw new ArgumentOutOfRangeException("numberToSelect");
+            }
 
-			if (numberToSelect == from.Count())
-			{
-				return new List<Firework>(from);
-			}
+            if (numberToSelect == from.Count())
+            {
+                return new List<Firework>(from);
+            }
 
-			List<Firework> selectedLocations = new List<Firework>(numberToSelect);
-			if (numberToSelect == 0)
-			{
-				return selectedLocations;
-			}
+            List<Firework> selectedLocations = new List<Firework>(numberToSelect);
+            if (numberToSelect == 0)
+            {
+                return selectedLocations;
+            }
 
-			// 1. Find a firework with best quality - it will be kept anyways
-			Firework bestFirework = bestFireworkSelector(from);
-			selectedLocations.Add(bestFirework);
+            // 1. Find a firework with best quality - it will be kept anyways
+            Firework bestFirework = this.bestFireworkSelector(from);
+            selectedLocations.Add(bestFirework);
 
             if (numberToSelect > 1)
             {
                 // 2. Calculate distances between all fireworks
-                IDictionary<Firework, double> distances = CalculateDistances(from);
+                IDictionary<Firework, double> distances = this.CalculateDistances(from);
 
                 // 3. Calculate probabilities for each firework
-                IDictionary<Firework, double> probabilities = CalculateProbabilities(distances);
+                IDictionary<Firework, double> probabilities = this.CalculateProbabilities(distances);
 
                 // 4. Select desiredLocationsNumber - 1 of fireworks based on the probabilities
                 IOrderedEnumerable<KeyValuePair<Firework, double>> sortedProbabilities = probabilities.OrderByDescending(p => p.Value, new DoubleExtensions.DoubleExtensionComparer());
@@ -94,35 +94,35 @@ namespace FireworksNet.Selection
                 selectedLocations.AddRange(otherSelectedLocations);
             }
 
-			return selectedLocations;
-		}
+            return selectedLocations;
+        }
 
         protected virtual IDictionary<Firework, double> CalculateDistances(IEnumerable<Firework> allCurrentFireworks)
-		{
-			Dictionary<Firework, double> distances = new Dictionary<Firework, double>(allCurrentFireworks.Count());
-			foreach (Firework firework in allCurrentFireworks)
-			{
-				distances.Add(firework, 0.0);
-				foreach (Firework otherFirework in allCurrentFireworks)
-				{
-					distances[firework] += distanceCalculator.Calculate(firework, otherFirework);
-				}
-			}
+        {
+            Dictionary<Firework, double> distances = new Dictionary<Firework, double>(allCurrentFireworks.Count());
+            foreach (Firework firework in allCurrentFireworks)
+            {
+                distances.Add(firework, 0.0);
+                foreach (Firework otherFirework in allCurrentFireworks)
+                {
+                    distances[firework] += this.distanceCalculator.Calculate(firework, otherFirework);
+                }
+            }
 
-			return distances;
-		}
+            return distances;
+        }
 
         protected virtual IDictionary<Firework, double> CalculateProbabilities(IDictionary<Firework, double> distances)
-		{
-			Dictionary<Firework, double> probabilities = new Dictionary<Firework, double>(distances.Count());
-			double distancesSum = distances.Values.Sum();
-			foreach (KeyValuePair<Firework, double> distance in distances)
-			{
-				double probability = distance.Value / distancesSum;
-				probabilities.Add(distance.Key, probability);
-			}
+        {
+            Dictionary<Firework, double> probabilities = new Dictionary<Firework, double>(distances.Count());
+            double distancesSum = distances.Values.Sum();
+            foreach (KeyValuePair<Firework, double> distance in distances)
+            {
+                double probability = distance.Value / distancesSum;
+                probabilities.Add(distance.Key, probability);
+            }
 
-			return probabilities;
-		}
-	}
+            return probabilities;
+        }
+    }
 }
