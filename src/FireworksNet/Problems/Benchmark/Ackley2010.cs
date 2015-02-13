b72b9bf0e4378c25f21eb4a0079b62246c5bffd2
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-02-13 14:17:52 +0300

    Added Ackley test function, per 2010 paper.

---
+++ src/FireworksNet/Problems/Benchmark/Ackley2010.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using FireworksNet.Model;
+
+namespace FireworksNet.Problems.Benchmark
+{
+    /// <summary>
+    /// Represents Ackley test function, as used in 2010 paper.
+    /// </summary>
+    /// <remarks>http://en.wikipedia.org/wiki/Test_functions_for_optimization</remarks>
+    public sealed class Ackley2010 : BenchmarkProblem
+    {
+        private const int dimensionality = 30;
+        private const double minDimensionValue = -100.0;
+        private const double maxDimensionValue = 100.0;
+        private const double minInitialDimensionValue = 30.0;
+        private const double maxInitialDimensionValue = 50.0;
+        private const double knownBestQuality = 0.0;
+        private const ProblemTarget problemTarget = ProblemTarget.Minimum;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Ackley2010"/> class.
+        /// </summary>
+        /// <param name="dimensions">Dimensions of the problem.</param>
+        /// <param name="initialDimensionRanges">Initial dimension ranges, to be used to 
+        /// create initial fireworks.</param>
+        /// <param name="targetFunction">Quality function.</param>
+        /// <param name="knownSolution">Known solution.</param>
+        /// <param name="stopCondition">Algorithm stop condition.</param>
+        /// <param name="target">Problem target.</param>
+        private Ackley2010(IList<Dimension> dimensions, IDictionary<Dimension, Range> initialDimensionRanges, Func<IDictionary<Dimension, double>, double> targetFunction, Solution knownSolution, ProblemTarget target)
+            : base(dimensions, initialDimensionRanges, targetFunction, knownSolution, target)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="Ackley2010"/> class.
+        /// </summary>
+        /// <returns><see cref="Ackley2010"/> instance that represents
+        /// Ackley test function, as used in 2010 paper.</returns>
+        public static Ackley2010 Create()
+        {
+            Dimension[] dimensions = new Dimension[Ackley2010.dimensionality];
+            IDictionary<Dimension, Range> initialDimensionRanges = new Dictionary<Dimension, Range>(Ackley2010.dimensionality);
+            IDictionary<Dimension, double> knownBestCoordinates = new Dictionary<Dimension, double>(Ackley2010.dimensionality);
+            for (int i = 0; i < Ackley2010.dimensionality; i++)
+            {
+                dimensions[i] = new Dimension(new Range(Ackley2010.minDimensionValue, Ackley2010.maxDimensionValue));
+                initialDimensionRanges.Add(dimensions[i], new Range(Ackley2010.minInitialDimensionValue, Ackley2010.maxInitialDimensionValue));
+                knownBestCoordinates.Add(dimensions[i], 0.0);
+            }
+
+            Func<IDictionary<Dimension, double>, double> func = new Func<IDictionary<Dimension, double>, double>(
+                (c) =>
+                {
+                    double firstSum = 0.0;
+                    double secondSum = 0.0;
+                    foreach(double value in c.Values)
+                    {
+                        firstSum += Math.Pow(value, 2.0);
+                        secondSum += Math.Cos(2.0 * Math.PI * Math.Pow(value, 2.0));
+                    }
+
+                    return 20.0 + Math.E - 20.0 * Math.Exp(-0.2 * Math.Sqrt((1 / Ackley2010.dimensionality) * firstSum)) - Math.Exp((1 / Ackley2010.dimensionality) * secondSum);
+                }
+            );
+
+            return new Ackley2010(dimensions, initialDimensionRanges, func, new Solution(knownBestCoordinates, Ackley2010.knownBestQuality), Ackley2010.problemTarget);
+        }
+    }
+}
