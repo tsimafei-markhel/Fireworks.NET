cade7eb8f7a12ad3750d4034c34a85bbb60f188d
unknown <Roman Shorin@.(none)>
2015-02-22 22:53:04 +0200

    additions

---
+++ src/FireworksNet/Selection/BestSelector.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FireworksNet.Extensions;
+using FireworksNet.Model;
+using System.Diagnostics;
+
+namespace FireworksNet.Selection
+{
+    public class BestSelector : ISelector
+    {
+        private readonly Func<IEnumerable<Firework>, Firework> bestFireworkSelector;
+        private readonly int samplingNumber;
+
+        public BestSelector(Func<IEnumerable<Firework>, Firework> bestFireworkSelector, int samplingNumber)
+        {
+            if (samplingNumber < 0)
+            {
+                throw new ArgumentOutOfRangeException("samplingNumber");
+            }
+
+            if (bestFireworkSelector == null)
+            {
+                throw new ArgumentNullException("bestFireworkSelector");
+            }
+
+            this.bestFireworkSelector = bestFireworkSelector;
+            this.samplingNumber = samplingNumber;
+        }
+
+        public BestSelector(Func<IEnumerable<Firework>, Firework> bestFireworkSelector)
+            : this(bestFireworkSelector, 0)
+        {
+        }
+
+        public virtual IEnumerable<Firework> Select(IEnumerable<Firework> from)
+        {
+            return this.Select(from, this.samplingNumber);
+        }
+
+        public virtual IEnumerable<Firework> Select(IEnumerable<Firework> from, int numberToSelect)
+        {
+            if (from == null)
+            {
+                throw new ArgumentNullException("from");
+            }
+
+            if (numberToSelect < 0)
+            {
+                throw new ArgumentOutOfRangeException("numberToSelect");
+            }
+
+            if (numberToSelect > from.Count())
+            {
+                throw new ArgumentOutOfRangeException("numberToSelect");
+            }
+
+            if (numberToSelect == from.Count())
+            {
+                return new List<Firework>(from);
+            }
+
+            List<Firework> bestFireworks = new List<Firework>(numberToSelect);
+            if (numberToSelect == 0)
+            {
+                return bestFireworks;
+            }
+
+            Debug.Assert(this.bestFireworkSelector != null, "Best firework selector is null");
+
+            if (numberToSelect >= 1)
+            {
+                // Find number of fireworks with best quality based on sampling number
+                IList<Firework> qualityLocations = this.SelectBestFireworks(from, numberToSelect);
+                bestFireworks.AddRange(qualityLocations);
+            }
+
+            return bestFireworks;
+        }
+
+        protected virtual IList<Firework> SelectBestFireworks(IEnumerable<Firework> allCurrentFireworks, int numberToSelect)
+        {
+            if (allCurrentFireworks == null)
+            {
+                throw new ArgumentNullException("allCurrentFireworks");
+            }
+
+            IList<Firework> currentFireworks = this.CopyFireworksCollection(allCurrentFireworks);           
+
+            List<Firework> qualityLocations = new List<Firework>(numberToSelect);
+            for (int i = 0; i < numberToSelect; i++)
+            {
+                Firework firework = this.bestFireworkSelector(currentFireworks);
+                qualityLocations.Add(firework);
+                currentFireworks.Remove(firework);
+            }
+            return qualityLocations;
+        }
+
+        protected virtual IList<Firework> CopyFireworksCollection(IEnumerable<Firework> currentFireworks)
+        {
+            List<Firework> copyCurrentFireworks = new List<Firework>(currentFireworks.Count());
+            copyCurrentFireworks.AddRange(currentFireworks);
+
+            return copyCurrentFireworks;
+        }
+    }
+}
---
+++ src/FireworksNet/Selection/RandomSelector.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using FireworksNet.Model;
+using FireworksNet.Random;
+using FireworksNet.Extensions;
+
+namespace FireworksNet.Selection
+{
+    public class RandomSelector : ISelector
+    {
+        private readonly System.Random randomizer;
+        private readonly int samplingNumber;
+
+        public RandomSelector(System.Random randomizer, int samplingNumber)
+        {
+            if (randomizer == null)
+            {
+                throw new ArgumentNullException("randomzier");
+            }
+
+            if (samplingNumber < 0)
+            {
+                throw new ArgumentOutOfRangeException("samplingNumber");
+            }
+
+            this.samplingNumber = samplingNumber;
+            this.randomizer = randomizer;
+        }
+
+        public RandomSelector(System.Random randomizer)
+            : this(randomizer, 0)
+        {
+        }
+
+        public virtual IEnumerable<Firework> Select(IEnumerable<Firework> from)
+        {
+            return this.Select(from, this.samplingNumber);
+        }
+
+        public virtual IEnumerable<Firework> Select(IEnumerable<Firework> from, int numberToSelect)
+        {
+            if (from == null)
+            {
+                throw new ArgumentNullException("from");
+            }
+
+            if (numberToSelect < 0)
+            {
+                throw new ArgumentOutOfRangeException("numberToSelect");
+            }
+
+            if (numberToSelect > from.Count())
+            {
+                throw new ArgumentOutOfRangeException("numberToSelect");
+            }
+
+            if (numberToSelect == from.Count())
+            {
+                return new List<Firework>(from);
+            }
+
+            List<Firework> selectedLocations = new List<Firework>(numberToSelect);
+            if (numberToSelect == 0)
+            {
+                return selectedLocations;
+            }
+
+            if (numberToSelect >= 1)
+            {
+                // 1. Generate number of fireworks using random
+                IEnumerable<int> generatedNumber = this.randomizer.NextInt32s(numberToSelect, 0, from.Count() - 1);
+                Debug.Assert(generatedNumber != null, "Generated numbers collection is null");
+
+                // 2. Find fireworks by generated numbers
+                foreach (int number in generatedNumber)
+                {
+                    Firework firework = from.ElementAt(number);
+                    Debug.Assert(firework != null, "Firework in null");
+
+                    selectedLocations.Add(firework);
+                }           
+            }
+
+            return selectedLocations;
+        }     
+    }
+}
+
