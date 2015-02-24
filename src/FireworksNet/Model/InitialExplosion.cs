a8be16663ced316645b807397cbf6f4131e88e5f
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-02-24 18:40:43 +0300

    Minor renaming

--- src/FireworksNet/Model/InitialExplosion.cs
+++ src/FireworksNet/Model/InitialExplosion.cs
@@ -13,26 +13,26 @@ namespace FireworksNet.Model
         /// Initializes a new instance of the <see cref="InitialExplosion"/> class.
         /// </summary>
         /// <param name="stepNumber">The number of step this explosion took place at.</param>
-        /// <param name="initialSparksCount">The number of sparks (i.e. initial fireworks)
+        /// <param name="initialSparksNumber">The number of sparks (i.e. initial fireworks)
         /// to be generated.</param>
-        /// <exception cref="System.ArgumentOutOfRangeException"> if <paramref name="initialSparksCount"/>
+        /// <exception cref="System.ArgumentOutOfRangeException"> if <paramref name="initialSparksNumber"/>
         /// is less than zero.</exception>
-        public InitialExplosion(int stepNumber, int initialSparksCount)
-            : base(stepNumber, new Dictionary<FireworkType, int>() { { FireworkType.Initial, initialSparksCount } })
+        public InitialExplosion(int stepNumber, int initialSparksNumber)
+            : base(stepNumber, new Dictionary<FireworkType, int>() { { FireworkType.Initial, initialSparksNumber } })
         {
-            if (initialSparksCount < 0)
+            if (initialSparksNumber < 0)
             {
-                throw new ArgumentOutOfRangeException("initialSparksCount");
+                throw new ArgumentOutOfRangeException("initialSparksNumber");
             }
         }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="InitialExplosion"/> class.
         /// </summary>
-        /// <param name="initialSparksCount">The number of sparks (i.e. initial fireworks)
+        /// <param name="initialSparksNumber">The number of sparks (i.e. initial fireworks)
         /// to be generated.</param>
-        public InitialExplosion(int initialSparksCount)
-            : this(0, initialSparksCount)
+        public InitialExplosion(int initialSparksNumber)
+            : this(0, initialSparksNumber)
         {
         }
     }
