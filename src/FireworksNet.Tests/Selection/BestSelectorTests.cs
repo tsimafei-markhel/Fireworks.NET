8b5fc1a8a32d1bf9951243f1add5653c97456455
RomanShorin <roma.shorin@gmail.com>
2015-03-09 14:39:14 +0200

    Change tests for BestSeletor

--- src/FireworksNet.Tests/Selection/BestSelectorTests.cs
+++ src/FireworksNet.Tests/Selection/BestSelectorTests.cs
@@ -8,11 +8,11 @@ namespace FireworksNet.Tests.Selection
 {
     public class BestSelectorTests
     {
-        private int samplingNumber;
-        private int countFireworks;
-        private Func<IEnumerable<Firework>, Firework> getBest;
-        private IEnumerable<Firework> allFireworks;
-        private BestSelector bestSelector;
+        private readonly int samplingNumber;
+        private readonly int countFireworks;
+        private readonly Func<IEnumerable<Firework>, Firework> getBest;
+        private readonly IEnumerable<Firework> allFireworks;
+        private readonly BestSelector bestSelector;
 
         public BestSelectorTests()
         {
@@ -49,7 +49,6 @@ namespace FireworksNet.Tests.Selection
         public void Select_PresentAllParam_ReturnsEqualFireworks()
         {
             IEnumerable<Firework> expectedFireworks = SelectorTestsHelper.BestFireworks;
-            this.samplingNumber++;
 
             IEnumerable<Firework> resultingFireworks = bestSelector.Select(this.allFireworks, this.samplingNumber);
 
@@ -61,7 +60,6 @@ namespace FireworksNet.Tests.Selection
         public void Select_PresentAllParam_ReturnsNonEqualFireworks()
         {
             IEnumerable<Firework> expectedFireworks = SelectorTestsHelper.NonBestFireworks;
-            this.samplingNumber++;
 
             IEnumerable<Firework> resultingFireworks = bestSelector.Select(this.allFireworks, this.samplingNumber);
 
@@ -73,45 +71,44 @@ namespace FireworksNet.Tests.Selection
         public void Select_NullAs1stParam_ExceptionThrown()
         {
             IEnumerable<Firework> currentFireworks = null;
-            string exceptionMessage = "Value cannot be null.\r\nParameter name: from";
+            string expectedParamName = "from";
 
-            Exception actualException = Assert.Throws<ArgumentNullException>(() => this.bestSelector.Select(currentFireworks));
+            ArgumentNullException actualException = Assert.Throws<ArgumentNullException>(() => this.bestSelector.Select(currentFireworks));
 
             Assert.NotNull(actualException);
-            Assert.Equal(exceptionMessage, actualException.Message);
+            Assert.Equal(expectedParamName, actualException.ParamName);
         }
 
         [Fact]
         public void Select_NegativeNumberAs2ndParam_ExceptionThrown()
         {
-            string exceptionMessage = "Specified argument was out of the range of valid values.\r\nParameter name: numberToSelect";
-            this.samplingNumber = -1;
+            string expectedParamName = "numberToSelect";
 
-            Exception actualException = Assert.Throws<ArgumentOutOfRangeException>(() => this.bestSelector.Select(this.allFireworks, this.samplingNumber));
-          
+            ArgumentOutOfRangeException actualException = Assert.Throws<ArgumentOutOfRangeException>(() => this.bestSelector.Select(this.allFireworks, -1));
+
             Assert.NotNull(actualException);
-            Assert.Equal(exceptionMessage, actualException.Message);
+            Assert.Equal(expectedParamName, actualException.ParamName);
         }
 
         [Fact]
         public void Select_GreatNumberAs2ndParam_ExceptionThrown()
         {
-            string exceptionMessage = "Specified argument was out of the range of valid values.\r\nParameter name: numberToSelect";
-            this.samplingNumber = this.countFireworks + 1;
+            string expectedParamName = "numberToSelect";
+            int samplingNumber = this.countFireworks + 1;
 
-            Exception actualException = Assert.Throws<ArgumentOutOfRangeException>(() => this.bestSelector.Select(this.allFireworks, this.samplingNumber));
+            ArgumentOutOfRangeException actualException = Assert.Throws<ArgumentOutOfRangeException>(() => this.bestSelector.Select(this.allFireworks, samplingNumber));
 
             Assert.NotNull(actualException);
-            Assert.Equal(exceptionMessage, actualException.Message);
+            Assert.Equal(expectedParamName, actualException.ParamName);
         }
 
         [Fact]
         public void Select_CountFireworksEqual2ndParam_ReturnsEqualFireworks()
         {
             IEnumerable<Firework> expectedFireworks = this.allFireworks;
-            this.samplingNumber = this.countFireworks;
+            int samplingNumber = this.countFireworks;
 
-            IEnumerable<Firework> resultingFireworks = bestSelector.Select(this.allFireworks, this.samplingNumber);
+            IEnumerable<Firework> resultingFireworks = bestSelector.Select(this.allFireworks, samplingNumber);
 
             Assert.NotSame(expectedFireworks, resultingFireworks);
             Assert.Equal(expectedFireworks, resultingFireworks);
@@ -121,9 +118,8 @@ namespace FireworksNet.Tests.Selection
         public void Select_NullAs2ndParam_ReturnsEmptyCollectionFireworks()
         {
             IEnumerable<Firework> expectedFireworks = new List<Firework>();
-            this.samplingNumber = 0;
 
-            IEnumerable<Firework> resultingFireworks = bestSelector.Select(this.allFireworks, this.samplingNumber);
+            IEnumerable<Firework> resultingFireworks = bestSelector.Select(this.allFireworks, 0);
 
             Assert.NotSame(expectedFireworks, resultingFireworks);
             Assert.Equal(expectedFireworks, resultingFireworks);
