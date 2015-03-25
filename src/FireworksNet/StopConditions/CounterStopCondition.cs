2eaccf64d86fe7b05fb41a2de15142a12c072ce9
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-03-25 16:16:26 +0300

    Added XML comments to some Stop Conditions classes.

--- src/FireworksNet/StopConditions/CounterStopCondition.cs
+++ src/FireworksNet/StopConditions/CounterStopCondition.cs
@@ -4,13 +4,26 @@ using FireworksNet.Model;
 
 namespace FireworksNet.StopConditions
 {
-    // Counts something; stops when count exceeds some threshold
+    /// <summary>
+    /// Counts something; stops when count exceeds some threshold.
+    /// </summary>
     public class CounterStopCondition : IStopCondition
     {
         private int count;
 
+        /// <summary>
+        /// Gets the threshold. Exceeding this threshold is a
+        /// stop condition for <see cref="CounterStopCondition"/>
+        /// user.
+        /// </summary>
         public int Threshold { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CounterStopCondition"/> class.
+        /// </summary>
+        /// <param name="threshold">The threshold.</param>
+        /// <exception cref="System.ArgumentOutOfRangeException"> if <paramref name="threshold"/>
+        /// is less than zero.</exception>
         public CounterStopCondition(int threshold)
         {
             if (threshold < 0)
@@ -22,16 +35,33 @@ namespace FireworksNet.StopConditions
             this.count = 0;
         }
 
+        /// <summary>
+        /// Tells if an algorithm that is currently in <paramref name="state"/> state
+        /// should stop (and don't make further steps) or not.
+        /// </summary>
+        /// <param name="state">The current algorithm state.</param>
+        /// <returns>
+        /// <c>true</c> if an algorithm that is currently in <paramref name="state"/>
+        /// state should stop (and don't make further steps). Otherwise <c>false</c>.
+        /// </returns>
         public virtual bool ShouldStop(AlgorithmState state)
         {
             return this.count >= this.Threshold;
         }
 
+        /// <summary>
+        /// Increments the internal counter.
+        /// </summary>
         public virtual void IncrementCounter()
         {
             Interlocked.Increment(ref this.count);
         }
 
+        /// <summary>
+        /// Increments the internal counter. Can be used as an event handler.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="eventArgs">The event arguments.</param>
         public virtual void IncrementCounter(object sender, object eventArgs)
         {
             IncrementCounter();
--- src/FireworksNet/StopConditions/StepCounterStopCondition.cs
+++ src/FireworksNet/StopConditions/StepCounterStopCondition.cs
@@ -3,13 +3,32 @@ using FireworksNet.Model;
 
 namespace FireworksNet.StopConditions
 {
+    /// <summary>
+    /// Counts algorithm steps; stops when count exceeds some threshold.
+    /// </summary>
     public class StepCounterStopCondition : CounterStopCondition
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StepCounterStopCondition"/> class.
+        /// </summary>
+        /// <param name="maxStepCount">The threshold - maximum step count.</param>
         public StepCounterStopCondition(int maxStepCount)
             : base(maxStepCount)
         {
         }
 
+        /// <summary>
+        /// Tells if an algorithm that is currently in <paramref name="state"/> state
+        /// should stop (and don't make further steps) or not. Takes step count from
+        /// the <paramref name="state"/>.
+        /// </summary>
+        /// <param name="state">The current algorithm state.</param>
+        /// <returns>
+        /// <c>true</c> if an algorithm that is currently in <paramref name="state"/>
+        /// state should stop (and don't make further steps). Otherwise <c>false</c>.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException"> if <paramref name="state"/>
+        /// is <c>null</c>.</exception>
         public override bool ShouldStop(AlgorithmState state)
         {
             bool shouldStop = base.ShouldStop(state);
