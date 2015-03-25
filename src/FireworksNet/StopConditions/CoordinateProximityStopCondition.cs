481a55c0d1afddd19b1feb9ceb6dfe292722470a
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2015-03-25 17:12:28 +0300

    Some proximity-based stop conditions.

---
+++ src/FireworksNet/StopConditions/CoordinateProximityStopCondition.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using FireworksNet.Distances;
+using FireworksNet.Extensions;
+using FireworksNet.Model;
+
+namespace FireworksNet.StopConditions
+{
+    /// <summary>
+    /// Stops when coordinates of some <see cref="Solution"/> (typically
+    /// the current best one) gets close enough to the coordinates of
+    /// the etalon <see cref="Solution"/>.
+    /// </summary>
+    public class CoordinateProximityStopCondition : ProximityStopCondition
+    {
+        /// <summary>
+        /// Gets the distance calculator.
+        /// </summary>
+        public IDistance DistanceCalculator { get; private set; }
+
+        /// <summary>
+        /// Gets the distance threshold.
+        /// </summary>
+        public double DistanceThreshold { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CoordinateProximityStopCondition"/> class.
+        /// </summary>
+        /// <param name="expectation">The the expected <see cref="Solution"/> (an etalon).</param>
+        /// <param name="distanceCalculator">The distance calculator.</param>
+        /// <param name="distanceThreshold">The distance threshold.</param>
+        /// <exception cref="System.ArgumentNullException"> if <paramref name="distanceCalculator"/>
+        /// is <c>null</c>.</exception>
+        /// <exception cref="System.ArgumentOutOfRangeException"> if <paramref name="distanceThreshold"/>
+        /// is <see cref="Double.NaN"/> or <see cref="Double.PositiveInfinity"/> or
+        /// <see cref="Double.NegativeInfinity"/>.</exception>
+        public CoordinateProximityStopCondition(Solution expectation, IDistance distanceCalculator, double distanceThreshold)
+            : base(expectation)
+        {
+            if (distanceCalculator == null)
+            {
+                throw new ArgumentNullException("distanceCalculator");
+            }
+
+            if (double.IsNaN(distanceThreshold) || double.IsInfinity(distanceThreshold))
+            {
+                throw new ArgumentOutOfRangeException("distanceThreshold");
+            }
+
+            this.DistanceCalculator = distanceCalculator;
+            this.DistanceThreshold = distanceThreshold;
+        }
+
+        /// <summary>
+        /// Tells if an algorithm that is currently in <paramref name="state"/> state
+        /// should stop (and don't make further steps) or not. Stops if the distance
+        /// between the <paramref name="state"/>'s Best Solution and the 
+        /// <see cref="CoordinateProximityStopCondition.Expectation"/> is less than
+        /// or equal to <see cref="CoordinateProximityStopCondition.DistanceThreshold"/>.
+        /// </summary>
+        /// <param name="state">The current algorithm state.</param>
+        /// <returns>
+        /// <c>true</c> if an algorithm that is currently in <paramref name="state"/>
+        /// state should stop (and don't make further steps). Otherwise <c>false</c>.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException"> if <paramref name="state"/>
+        /// is <c>null</c>.</exception>
+        public override bool ShouldStop(AlgorithmState state)
+        {
+            if (state == null)
+            {
+                throw new ArgumentNullException("state");
+            }
+
+            double distance = this.DistanceCalculator.Calculate(state.BestSolution, this.Expectation);
+            return distance.IsLessOrEqual(this.DistanceThreshold);
+        }
+    }
+}
---
+++ src/FireworksNet/StopConditions/QualityProximityStopCondition.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using FireworksNet.Extensions;
+using FireworksNet.Model;
+
+namespace FireworksNet.StopConditions
+{
+    /// <summary>
+    /// Stops when quality of some <see cref="Solution"/> (typically
+    /// the current best one) gets close enough to the quality of
+    /// the etalon <see cref="Solution"/>.
+    /// </summary>
+    public class QualityProximityStopCondition : ProximityStopCondition
+    {
+        /// <summary>
+        /// Gets the quality threshold.
+        /// </summary>
+        public double QualityThreshold { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QualityProximityStopCondition"/> class.
+        /// </summary>
+        /// <param name="expectation">The the expected <see cref="Solution"/> (an etalon).</param>
+        /// <param name="qualityThreshold">The quality threshold.</param>
+        /// <exception cref="System.ArgumentOutOfRangeException"> if <paramref name="qualityThreshold"/>
+        /// is <see cref="Double.NaN"/> or <see cref="Double.PositiveInfinity"/> or
+        /// <see cref="Double.NegativeInfinity"/>.</exception>
+        public QualityProximityStopCondition(Solution expectation, double qualityThreshold)
+            : base(expectation)
+        {
+            if (double.IsNaN(qualityThreshold) || double.IsInfinity(qualityThreshold))
+            {
+                throw new ArgumentOutOfRangeException("qualityThreshold");
+            }
+
+            this.QualityThreshold = qualityThreshold;
+        }
+
+        /// <summary>
+        /// Tells if an algorithm that is currently in <paramref name="state"/> state
+        /// should stop (and don't make further steps) or not. Stops if absolute value
+        /// of the difference between the quality of <paramref name="state"/>'s Best
+        /// Solution and <see cref="QualityProximityStopCondition.Expectation"/> is less
+        /// than or equal to <see cref="QualityProximityStopCondition.QualityThreshold"/>.
+        /// </summary>
+        /// <param name="state">The current algorithm state.</param>
+        /// <returns>
+        /// <c>true</c> if an algorithm that is currently in <paramref name="state"/>
+        /// state should stop (and don't make further steps). Otherwise <c>false</c>.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException"> if <paramref name="state"/>
+        /// is <c>null</c>.</exception>
+        public override bool ShouldStop(AlgorithmState state)
+        {
+            if (state == null)
+            {
+                throw new ArgumentNullException("state");
+            }
+
+            double qualityProximity = Math.Abs(state.BestSolution.Quality - Expectation.Quality);
+            return qualityProximity.IsLessOrEqual(this.QualityThreshold);
+        }
+    }
+}
