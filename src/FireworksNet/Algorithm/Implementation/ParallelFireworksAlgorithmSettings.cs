d41cc9b0ce88b70385271b2273da353299821ca6
sleshJdev <slesh.eugene93@gmail.com>
2015-04-01 19:43:05 +0200

    Preparing for pull request

---
+++ src/FireworksNet/Algorithm/Implementation/ParallelFireworksAlgorithmSettings.cs
@@ -0,0 +1,27 @@
+﻿namespace FireworksNet.Algorithm.Implementation
+{
+    /// <summary>
+    /// Stores user-defined constants that control algorithm run.
+    /// </summary>
+    /// <remarks>Uses original notation from paper</remarks>
+    public class ParallelFireworksAlgorithmSettings
+    {
+        /// <summary>
+        /// m - Fixed quantity of spark, which generates each firework.
+        /// m recommended be 16 or multiple of 16
+        /// </summary>
+        public int FixedQuantitySparks { set; get; }
+
+        /// <summary>
+        /// A - Maximum explosion amplitude. 
+        /// A recommended take 1. 
+        /// </summary>
+        public double Amplitude { get; set; }        
+
+        /// <summary>
+        /// δ - Constant for scaling factor which is multiplied distance by each dimension. 
+        /// δ should take (0; 1)
+        /// </summary>
+        public double Delta { get; set; }
+    }
+}
