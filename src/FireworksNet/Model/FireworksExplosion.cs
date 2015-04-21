31a89c8272fc3f6572ba33a0099be6db24a836b7
RomanShorin <roma.shorin@gmail.com>
2015-04-21 16:34:44 +0300

    Add EliteStrategyGenerator and FireworksExplosion

---
+++ src/FireworksNet/Model/FireworksExplosion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FireworksNet.Model
+{
+    public class FireworksExplosion : ExplosionBase
+    {
+        public IEnumerable<Firework> Fireworks { get; set; }
+
+        public FireworksExplosion(int stepNumber, int fireworksNumber, IEnumerable<Firework> fireworks)
+            : base(stepNumber, new Dictionary<FireworkType, int>() { { FireworkType.ExplosionSpark, fireworksNumber } })
+        {
+            if (fireworksNumber < 0)
+            {
+                throw new ArgumentOutOfRangeException("fireworksNumber");
+            }
+
+            if (fireworks == null)
+            {
+                throw new ArgumentNullException("fireworks");
+            }
+
+            this.Fireworks = fireworks;
+        }
+    }
+}
--- src/FireworksNet/TempEliteStrategy.cs
+++ src/FireworksNet/TempEliteStrategy.cs
@@ -4,17 +4,33 @@ using FireworksNet.Model;
 using FireworksNet.Fit;
 using FireworksNet.Solving;
 using FireworksNet.Differentiation;
+using FireworksNet.Generation;
 
 namespace FireworksNet
 {
-    public class FirstOrderEliteStrategy : TempEliteStrategy
+    /// <summary>
+    /// Elite strategy spark generator using first order functions, per 2012 paper.
+    /// </summary>
+    public class FirstOrderEliteStrategy : EliteStrategyGenerator
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FirstOrderEliteStrategy"/> class.
+        /// </summary>
+        /// <param name="dimensions">The dimensions to fit generated sparks into.</param>
+        /// <param name="polynomialFit">The polynomial fit.</param>
         public FirstOrderEliteStrategy(IEnumerable<Dimension> dimensions, IFit polynomialFit)
             : base(dimensions, polynomialFit)
         {
         }
 
-        public override double SelectElitePoint(Func<double, double> func, Range variationRange)
+        /// <summary>
+        /// Calculates elite point by searching midpoint.
+        /// </summary>
+        /// <param name="func">The function to calculate elite point.</param>
+        /// <param name="variationRange">Represents an interval to calculate 
+        /// elite point.</param>
+        /// <returns>The coordinate of elite point.</returns>
+        protected override double CalculateElitePoint(Func<double, double> func, Range variationRange)
         {
             if (func == null)
             {
@@ -31,11 +47,24 @@ namespace FireworksNet
         }
     }
 
-    public class SecondOrderEliteStrategy : TempEliteStrategy
+    /// <summary>
+    /// Elite strategy spark generator using first order functions, per 2012 paper.
+    /// </summary>
+    public class SecondOrderEliteStrategy : EliteStrategyGenerator
     {
         private readonly IDifferentiator differentiation;
         private readonly ISolver solver;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SecondOrderEliteStrategy"/> class.
+        /// </summary>
+        /// <param name="dimensions">The dimensions to fit generated sparks into.</param>
+        /// <param name="polynomialFit">The polynomial fit.</param>
+        /// <param name="differentiation">A function differentiator.</param>
+        /// <param name="solver">A polynomial function solver.</param>
+        /// <exception cref="System.ArgumentNullException"> if <paramref name="differentiation"/>
+        /// or <paramref name="solver"/> is <c>null</c>.
+        /// </exception>
         public SecondOrderEliteStrategy(IEnumerable<Dimension> dimensions, IFit polynomialFit, IDifferentiator differentiation, ISolver solver)
             : base(dimensions, polynomialFit)
         {
@@ -53,7 +82,15 @@ namespace FireworksNet
             this.solver = solver;
         }
 
-        public override double SelectElitePoint(Func<double, double> func, Range variationRange)
+        /// <summary>
+        /// Calculates elite point with help differentiating <paramref name="func"/> 
+        /// and solving the resulting function.
+        /// </summary>
+        /// <param name="func">The function to calculate elite point.</param>
+        /// <param name="variationRange">Represents an interval to calculate 
+        /// elite point.</param>
+        /// <returns>The coordinate of elite point.</returns>
+        protected override double CalculateElitePoint(Func<double, double> func, Range variationRange)
         {
             if (func == null)
             {
@@ -68,89 +105,6 @@ namespace FireworksNet
             Func<double, double> derivative = this.differentiation.Differentiate(func);
 
             return solver.Solve(derivative, variationRange);
-        }
-    }
-
-    public abstract class TempEliteStrategy
-    {
-        private readonly IEnumerable<Dimension> dimensions;
-        private readonly IFit polynomialFit;
-
-        public FireworkType ElitePointType { get; set; }
-
-        protected TempEliteStrategy(IEnumerable<Dimension> dimensions, IFit polynomialFit)
-        {
-            if (dimensions == null)
-            {
-                throw new ArgumentNullException("dimensions");
-            }
-
-            if (polynomialFit == null)
-            {
-                throw new ArgumentNullException("polynomialFit");
-            }
-
-            this.dimensions = dimensions;
-            this.polynomialFit = polynomialFit;
-            this.ElitePointType = FireworkType.EliteFirework;
-        }
-
-        public abstract double SelectElitePoint(Func<double, double> func, Range variationRange);
-
-        public Firework GetFirework(IEnumerable<Firework> from, int birthStepNumber)
-        {
-            if (from == null)
-            {
-                throw new ArgumentNullException("from");
-            }
-
-            if (birthStepNumber < 0)
-            {
-                throw new ArgumentOutOfRangeException("birthStepNumber");
-            }
-
-            List<Firework> currentFireworks = new List<Firework>(from);
-
-            //11. Approximate fitness landscape in each projected one dimensional search space
-            double[] qualities = new double[currentFireworks.Count];
-
-            //Get qualities of the fireworks
-            int current = 0;
-            foreach (Firework firework in currentFireworks)
-            {
-                qualities[current] = firework.Quality;
-                current++;
-            }
-
-            Dictionary<Dimension, Func<double, double>> fitnessLandscapes = new Dictionary<Dimension, Func<double, double>>();
-
-            foreach (Dimension dimension in this.dimensions)
-            {
-                double[] coordinates = new double[currentFireworks.Count];
-
-                //Get coordinates of the fireworks in current dimension
-                current = 0;
-                foreach (Firework firework in currentFireworks)
-                {
-                    coordinates[current] = firework.Coordinates[dimension];
-                    current++;
-                }
-
-                Func<double, double> polynomial = this.polynomialFit.Approximate(coordinates, qualities);
-                fitnessLandscapes[dimension] = polynomial;
-            }
-
-            //12. Obtain a spark from approximated curves by Elite Strategy
-
-            Dictionary<Dimension, double> coordinatesElitePoint = new Dictionary<Dimension, double>();
-
-            foreach (KeyValuePair<Dimension, Func<double, double>> data in fitnessLandscapes)
-            {
-                double elitePoint = this.SelectElitePoint(data.Value, data.Key.VariationRange);
-                coordinatesElitePoint[data.Key] = elitePoint;
-            }
-
-            return new Firework(this.ElitePointType, birthStepNumber, coordinatesElitePoint);
         }
     }
 }
